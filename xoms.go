1c51815d529d4e575c678e73e728783dd75e60ce
maxho82 <maxho82@mail.ru>
2023-04-23 20:32:07 +0400

    00000

--- xoms.go
+++ xoms.go
@@ -1,8 +1,28 @@
 package xoms
 
 //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
-import "fmt"
+
+import (
+	"fmt"
+	"io/ioutil"
+	//"os"
+	//"path/filepath"
+)
 
 func Hello() {
 	fmt.Println("Hello, World!")
+}
+
+func listDirByReadDir(path string) {
+	lst, err := ioutil.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+	for _, val := range lst {
+		if val.IsDir() {
+			fmt.Printf("[%s]\n", val.Name())
+		} else {
+			fmt.Println(val.Name())
+		}
+	}
 }
